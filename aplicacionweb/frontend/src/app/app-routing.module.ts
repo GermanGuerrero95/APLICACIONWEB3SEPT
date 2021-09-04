0c7ce0d322cb42f6a38fa29ba3be99d0f348b864
Germán Guerrero <87212199+GermanGuerrero95@users.noreply.github.com>
2021-09-03 23:18:10 -0500

    Add files via upload

---
+++ aplicacionweb/frontend/src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { NuevoComponent } from './components/nuevo/nuevo.component';
+import { Error404Component } from './components/error404/error404.component';
+import { AboutComponent } from './components/about/about.component';
+import { MeComponent } from './components/me/me.component';
+
+const routes: Routes = [
+  {path:"",redirectTo:"/home",pathMatch:"full"},
+  {path:"home",component:HomeComponent},
+  {path:"about",component:AboutComponent},
+  {path:"me",component:MeComponent},
+  {path:"error404",component: Error404Component},
+  {path:"**",redirectTo:"/error404",pathMatch:"full"}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
